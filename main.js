61dad1c23145725071f2a1327affe517dc41bdad
WooYong333 <81574524+WooYong333@users.noreply.github.com>
2021-10-29 12:43:05 +0900

    First commit

---
+++ main.js
@@ -0,0 +1,9 @@
+const Discord = require("discord.js");
+
+const client = new Discord.Client();
+
+client.once("ready", () => {
+    console.log("I am ready");
+})
+
+client.login("OTAzNDU4NDYzNTM4MTAyMjcz.YXtRQw.UZMjSmn7KJvcTXmpZPqmDJu975I");
